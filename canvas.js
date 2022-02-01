ff66b0ce71b210d42ba2a9f9be131ae2a626b980
Daniel Altamirano <ciarmail@gmail.com>
2022-01-31 22:43:53 -0500

    Dibujada la orca y el cuerpo del muñeco

--- canvas.js
+++ canvas.js
@@ -15,7 +15,7 @@ function dTablero() {
     pincel.lineTo(50,590);
     pincel.fill();
 
-    //pincel.beginPath();
+    //dibujo lados de base
     pincel.moveTo(150,590);
     pincel.lineTo(125,550);
     pincel.lineTo(125,590);
@@ -42,15 +42,80 @@ function dGuiones(palabra) {
      }
 }
 
-
-function dLetra(letra) {
-    console.log(inicio);
+//PINTAR LA O LAS LETRAS CORRECTAS
+function dLetra(x, y, letra) {
     pincel.font="30pt Verdana";
 	pincel.fillStyle = "white";
-	pincel.fillText("T", inicio + 5, ybase + 40);
+	pincel.fillText(letra, x + 5, ybase + 40);
 }
 
+function dAhorcado(paso) {
+    pincel.fillStyle = "lightsalmon";
+    pincel.fillRect(75, 300, 25, 250);
+
+    pincel.fillStyle = "lightsalmon";
+    pincel.fillRect(75, 50, 25, 250);
+
+    pincel.fillStyle = "lightsalmon";
+    pincel.fillRect(75, 50, 200, 25);
+
+    pincel.fillStyle = "lightsalmon";
+    pincel.fillRect(270, 50, 10, 150);
+
+    //cabeza
+    pincel.fillStyle = "lightblue";
+    pincel.beginPath();
+    pincel.arc(275, 240, 40,0,Math.PI * 2);
+    pincel.fill();
+    pincel.beginPath();
+    pincel.fillStyle = "white";
+    //ojo izquierdo
+    pincel.arc(260, 230, 10, 0, Math.PI * 2);
+    //ojo derecho
+    pincel.arc(290, 230, 10, 0, Math.PI * 2);
+    pincel.fill();
+    //boca
+    pincel.beginPath();
+    pincel.ellipse(275, 260, 15, 15, 0, 0, Math.PI *2);
+    pincel.fill();
+
+    //cuerpo
+    pincel.beginPath();
+    pincel.fillStyle = "blue";
+    pincel.fillRect(270, 280, 10, 125);
+
+    //braso derecho
+    pincel.moveTo(280,290);
+    pincel.lineTo(375,230);
+    pincel.lineTo(375,240);
+    pincel.lineTo(280,300);
+    pincel.fill();
+
+    //braso izquierdo
+    pincel.moveTo(275,290);
+    pincel.lineTo(185,235);
+    pincel.lineTo(185,245);
+    pincel.lineTo(275,300);
+    pincel.fill();
+
+
+    //pie derecho
+    pincel.moveTo(270,400);
+    pincel.lineTo(375,475);
+    pincel.lineTo(375,480);
+    pincel.lineTo(270,410);
+    pincel.fill();
+
+    //pie izquierdo
+    pincel.moveTo(270,400);
+    pincel.lineTo(175,475);
+    pincel.lineTo(175,480);
+    pincel.lineTo(270,410);
+    pincel.fill();
+}
 
 dTablero();
-dGuiones("PRUEBA");
+//dGuiones("JAVASCRIPT12");
 //dLetra("P");
+
+dAhorcado();
